387965556e76817327755cef314b46f9604088c5
SimonHe <SimonHe@dhcp035-175-214.wireless.buffalo.edu>
2016-03-10 18:53:11 -0500

    Finished the set_pawnPosition test

--- DragonTeam/src/boardTests/Set_pawnPositionTests.java
+++ DragonTeam/src/boardTests/Set_pawnPositionTests.java
@@ -7,7 +7,7 @@ import org.junit.Test;
 import code.Board;
 
 //note
-public class set_pawnPositionTests {
+public class Set_pawnPositionTests {
 	
 	@Test public void test01(){
 		Board board = new Board();
@@ -16,16 +16,13 @@ public class set_pawnPositionTests {
 		board.set_pawnPosition(playerNO, position);
 		int[] expected = {2,3};
 		int[] actual = board.get_pawnPosition(playerNO);
+		int counter = 0;
+		for(int i = 0; i<2; i++){
+			if(expected[i] == actual[i]){
+				counter++;
+			}
+		}
 		assertTrue("Expected position: "+expected+" actual"
-				+ " position: "+actual,expected==actual);
+				+ " position: "+actual,counter==2);
 	}
-public static void main(String []args){
-	Board board = new Board();
-	int playerNO = 1;
-	int[] position = {2,3};
-	board.set_pawnPosition(playerNO, position);
-	int[] expected = {2,3};
-	int[] actual = board.get_pawnPosition(playerNO);
-	System.out.println(actual[0]);
-}
 }

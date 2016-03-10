44e10b45fa9321a9e9c7670d08720174c99d02a5
Joe <Joe@Yoface>
2016-03-10 15:17:28 -0500

    Added board constructor

--- DragonTeam/src/boardTests/set_pawnPositionTests.java
+++ DragonTeam/src/boardTests/set_pawnPositionTests.java
@@ -1,5 +1,15 @@
 package boardTests;
+
+import org.junit.Test;
+
+import code.Board;
+
 //note
 public class set_pawnPositionTests {
+	
+	@Test public void test01(){
+		Board board = new Board();
+		
+	}
 
 }

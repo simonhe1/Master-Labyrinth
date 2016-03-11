492e60feec9177a6e541363ebd7ddc4208d580c7
SimonHe <SimonHe@dhcp035-175-214.wireless.buffalo.edu>
2016-03-10 19:13:40 -0500

    Working on get_tokenPosition and set_tokenPosition tests

--- DragonTeam/src/boardTests/Get_tokenPositionTests.java
+++ DragonTeam/src/boardTests/Get_tokenPositionTests.java
@@ -1,5 +1,136 @@
 package boardTests;
 
-public class Get_tokenPositionTests {
+import static org.junit.Assert.assertTrue;
 
+import org.junit.Test;
+
+import code.Board;
+
+public class Get_tokenPositionTests {
+	@Test public void test01(){
+		Board board = new Board();
+		int input = 1;
+		int[] expected = {1,1};
+		int[] actual = board.get_tokenPosition(input);
+		int counter = 0;
+		for(int i = 0; i<2; i++){
+			if(expected[i] == actual[i]){
+				counter++;
+			}
+		}
+		assertTrue("Expected: "+expected+" Actual: "+actual,counter==2);
+		
+	}
+	@Test public void test02(){
+		Board board = new Board();
+		int input = 2;
+		int[] expected = {1,2};
+		int[] actual = board.get_tokenPosition(input);
+		int counter = 0;
+		for(int i = 0; i<2; i++){
+			if(expected[i] == actual[i]){
+				counter++;
+			}
+		}
+		assertTrue("Expected: "+expected+" Actual: "+actual,counter==2);
+		
+	}
+	@Test public void test03(){
+		Board board = new Board();
+		int input = 3;
+		int[] expected = {1,3};
+		int[] actual = board.get_tokenPosition(input);
+		int counter = 0;
+		for(int i = 0; i<2; i++){
+			if(expected[i] == actual[i]){
+				counter++;
+			}
+		}
+		assertTrue("Expected: "+expected+" Actual: "+actual,counter==2);
+		
+	}
+	@Test public void test04(){
+		Board board = new Board();
+		int input = 4;
+		int[] expected = {1,4};
+		int[] actual = board.get_tokenPosition(input);
+		int counter = 0;
+		for(int i = 0; i<2; i++){
+			if(expected[i] == actual[i]){
+				counter++;
+			}
+		}
+		assertTrue("Expected: "+expected+" Actual: "+actual,counter==2);
+		
+	}
+	@Test public void test05(){
+		Board board = new Board();
+		int input = 5;
+		int[] expected = {1,5};
+		int[] actual = board.get_tokenPosition(input);
+		int counter = 0;
+		for(int i = 0; i<2; i++){
+			if(expected[i] == actual[i]){
+				counter++;
+			}
+		}
+		assertTrue("Expected: "+expected+" Actual: "+actual,counter==2);
+		
+	}
+	@Test public void test06(){
+		Board board = new Board();
+		int input = 6;
+		int[] expected = {2,1};
+		int[] actual = board.get_tokenPosition(input);
+		int counter = 0;
+		for(int i = 0; i<2; i++){
+			if(expected[i] == actual[i]){
+				counter++;
+			}
+		}
+		assertTrue("Expected: "+expected+" Actual: "+actual,counter==2);
+		
+	}
+	@Test public void test07(){
+		Board board = new Board();
+		int input = 7;
+		int[] expected = {2,3};
+		int[] actual = board.get_tokenPosition(input);
+		int counter = 0;
+		for(int i = 0; i<2; i++){
+			if(expected[i] == actual[i]){
+				counter++;
+			}
+		}
+		assertTrue("Expected: "+expected+" Actual: "+actual,counter==2);
+		
+	}
+	@Test public void test08(){
+		Board board = new Board();
+		int input = 1;
+		int[] expected = {2,5};
+		int[] actual = board.get_tokenPosition(input);
+		int counter = 0;
+		for(int i = 0; i<2; i++){
+			if(expected[i] == actual[i]){
+				counter++;
+			}
+		}
+		assertTrue("Expected: "+expected+" Actual: "+actual,counter==2);
+		
+	}
+	@Test public void test09(){
+		Board board = new Board();
+		int input = 9;
+		int[] expected = {3,1};
+		int[] actual = board.get_tokenPosition(input);
+		int counter = 0;
+		for(int i = 0; i<2; i++){
+			if(expected[i] == actual[i]){
+				counter++;
+			}
+		}
+		assertTrue("Expected: "+expected+" Actual: "+actual,counter==2);
+		
+	}
 }
--- DragonTeam/src/boardTests/Set_tokenPositionTests.java
+++ DragonTeam/src/boardTests/Set_tokenPositionTests.java
@@ -1,5 +1,24 @@
 package boardTests;
 
-public class Set_tokenPositionTests {
+import static org.junit.Assert.assertTrue;
 
+import org.junit.Test;
+
+import code.Board;
+
+public class Set_tokenPositionTests {
+	@Test public void test01(){
+		Board board = new Board();
+		int input = 1;
+		int[] expected = {2,2};
+		int[] actual = board.get_tokenPosition(input);
+		int counter = 0;
+		for(int i = 0; i<2; i++){
+			if(expected[i] == actual[i]){
+				counter++;
+			}
+		}
+		assertTrue("Expected: "+expected+" Actual: "+actual,counter==2);
+		
+	}
 }

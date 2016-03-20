d78b42c503225983280db1d8167295ad72acec98
SimonHe <SimonHe@simons-mbp>
2016-03-20 09:40:30 -0400

    Finished get_tokenposition test

--- DragonTeam/src/boardTests/Get_tokenPositionTests.java
+++ DragonTeam/src/boardTests/Get_tokenPositionTests.java
@@ -10,10 +10,12 @@ public class Get_tokenPositionTests {
 	@Test public void test01(){
 		Board board = new Board();
 		int input = 1;
-		int row = 0;
-		int col = 0;
-		/*while(row!=)*/
 		int[] expected = {1,1};
+		board.set_tokenPosition(input,expected);//already checked the set_tokenPosition works 
+												//I'm setting position instead of just getting the actual
+												//position of the token because the tokens are scrambled
+												//initially and all we need to do is check if the get
+												//method actually works
 		int[] actual = board.get_tokenPosition(input);
 		int counter = 0;
 		for(int i = 0; i<2; i++){
@@ -22,83 +24,13 @@ public class Get_tokenPositionTests {
 			}
 		}
 		assertTrue("Expected: "+expected+" Actual: "+actual,counter==2);
-		
 	}
 	@Test public void test02(){
 		Board board = new Board();
 		int input = 2;
-		int[] expected = {1,2};
-		int[] actual = board.get_tokenPosition(input);
-		int counter = 0;
-		for(int i = 0; i<2; i++){
-			if(expected[i] == actual[i]){
-				counter++;
-			}
-		}
-		assertTrue("Expected: "+expected+" Actual: "+actual,counter==2);
-		
-	}
-	@Test public void test03(){
-		Board board = new Board();
-		int input = 3;
-		int[] expected = {1,3};
-		int[] actual = board.get_tokenPosition(input);
-		int counter = 0;
-		for(int i = 0; i<2; i++){
-			if(expected[i] == actual[i]){
-				counter++;
-			}
-		}
-		assertTrue("Expected: "+expected+" Actual: "+actual,counter==2);
-		
-	}
-	@Test public void test04(){
-		Board board = new Board();
-		int input = 4;
-		int[] expected = {1,4};
-		int[] actual = board.get_tokenPosition(input);
-		int counter = 0;
-		for(int i = 0; i<2; i++){
-			if(expected[i] == actual[i]){
-				counter++;
-			}
-		}
-		assertTrue("Expected: "+expected+" Actual: "+actual,counter==2);
-		
-	}
-	@Test public void test05(){
-		Board board = new Board();
-		int input = 5;
-		int[] expected = {1,5};
-		int[] actual = board.get_tokenPosition(input);
-		int counter = 0;
-		for(int i = 0; i<2; i++){
-			if(expected[i] == actual[i]){
-				counter++;
-			}
-		}
-		assertTrue("Expected: "+expected+" Actual: "+actual,counter==2);
-		
-	}
-	@Test public void test06(){
-		Board board = new Board();
-		int input = 6;
-		int[] expected = {2,1};
-		int[] actual = board.get_tokenPosition(input);
-		int counter = 0;
-		for(int i = 0; i<2; i++){
-			if(expected[i] == actual[i]){
-				counter++;
-			}
-		}
-		assertTrue("Expected: "+expected+" Actual: "+actual,counter==2);
-		
-	}
-	@Test public void test07(){
-		Board board = new Board();
-		int input = 7;
 		int[] expected = {2,3};
-		int[] actual = board.get_tokenPosition(input);
+		board.set_tokenPosition(input, expected);
+		int [] actual = board.get_tokenPosition(input);
 		int counter = 0;
 		for(int i = 0; i<2; i++){
 			if(expected[i] == actual[i]){
@@ -106,34 +38,5 @@ public class Get_tokenPositionTests {
 			}
 		}
 		assertTrue("Expected: "+expected+" Actual: "+actual,counter==2);
-		
-	}
-	@Test public void test08(){
-		Board board = new Board();
-		int input = 1;
-		int[] expected = {2,5};
-		int[] actual = board.get_tokenPosition(input);
-		int counter = 0;
-		for(int i = 0; i<2; i++){
-			if(expected[i] == actual[i]){
-				counter++;
-			}
-		}
-		assertTrue("Expected: "+expected+" Actual: "+actual,counter==2);
-		
-	}
-	@Test public void test09(){
-		Board board = new Board();
-		int input = 9;
-		int[] expected = {3,1};
-		int[] actual = board.get_tokenPosition(input);
-		int counter = 0;
-		for(int i = 0; i<2; i++){
-			if(expected[i] == actual[i]){
-				counter++;
-			}
-		}
-		assertTrue("Expected: "+expected+" Actual: "+actual,counter==2);
-		
 	}
 }

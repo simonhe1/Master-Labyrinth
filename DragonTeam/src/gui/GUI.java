e0999932c50a7d411828471a50fcb431f6c4930b
SimonHe <SimonHe@10.84.8.15>
2016-03-28 15:29:11 -0400

    Fixed parts of the scores to ensure we only play with the right
    amount of people

--- DragonTeam/src/gui/GUI.java
+++ DragonTeam/src/gui/GUI.java
@@ -16,35 +16,35 @@ import code.Board;
 public class GUI implements Runnable, Observer{
 	private Board _board;
 	private JFrame _window;
-	private JLabel _player1 = new JLabel();
-	private JLabel _player2 = new JLabel();
-	private JLabel _player3 = new JLabel();
-	private JLabel _player4 = new JLabel();
+	private JLabel _name1 = new JLabel();
+	private JLabel _name2 = new JLabel();
+	private JLabel _name3 = new JLabel();
+	private JLabel _name4 = new JLabel();
 	private JPanel _playerPanel = new JPanel();
 	private JPanel _boardPanel = new JPanel(); 
 	private JPanel _extraPanel = new JPanel();
 	private JButton _extra = new JButton();
-	private int _score1=0,_score2=0,_score3=0,_score4=0;
+	private int _score1=-1,_score2=-1,_score3=-1,_score4=-1;
 	public GUI(String p1,String p2,String p3,String p4){
 		_board = new Board();
 		_extra.setText(Integer.toString(_board.get_ExtraTile().type()));
-		_player1.setText(p1+"     "+_score1);
-		_player2.setText(p2+"     "+_score2);
-		_player3.setText(p3+"     "+_score3);
-		_player4.setText(p4+"     "+_score4);
+		_name1.setText(p1+"     "+(_score1+1));
+		_name2.setText(p2+"     "+(_score2+1));
+		_name3.setText(p3+"     "+(_score3+1));
+		_name4.setText(p4+"     "+(_score4+1));
 	}
 	public GUI(String p1,String p2,String p3){
 		_board = new Board();
 		_extra.setText(Integer.toString(_board.get_ExtraTile().type()));
-		_player1.setText(p1+"     "+_score1);
-		_player2.setText(p2+"     "+_score2);
-		_player3.setText(p3+"     "+_score3);
+		_name1.setText(p1+"     "+(_score1+1));
+		_name2.setText(p2+"     "+(_score2+1));
+		_name3.setText(p3+"     "+(_score3+1));
 	}
 	public GUI(String p1,String p2){
 		_board = new Board();
 		_extra.setText(Integer.toString(_board.get_ExtraTile().type()));
-		_player1.setText(p1+"     "+_score1);
-		_player2.setText(p2+"     "+_score2);
+		_name1.setText(p1+"     "+(_score1+1));
+		_name2.setText(p2+"     "+(_score2+1));
 	}
 	//public GUI(int n1,int n2,int n3,int n4,int n5){
 	//	_board = new Board(n1,n2,n3,n4,n5);
@@ -58,10 +58,10 @@ public class GUI implements Runnable, Observer{
 		_window.setVisible(true);
 		_window.setFocusable(true);
 		_playerPanel.setLayout(new BoxLayout(_playerPanel,BoxLayout.PAGE_AXIS));
-		_playerPanel.add(_player1);
-		_playerPanel.add(_player2);
-		_playerPanel.add(_player3);
-		_playerPanel.add(_player4);
+		_playerPanel.add(_name1);
+		_playerPanel.add(_name2);
+		_playerPanel.add(_name3);
+		_playerPanel.add(_name4);
 		_window.add(_playerPanel);
 		_boardPanel.setLayout(new GridLayout(7,7));
 		for(int i=0;i<=6;i++){
@@ -79,10 +79,10 @@ public class GUI implements Runnable, Observer{
 	}
 	@Override
 	public void update(Observable o, Object arg) { //I'm not sure what the parameters do, will research
-		_score1 = _board.showScore(1).size();
-		_score2 = _board.showScore(2).size();
-		_score3 = _board.showScore(3).size();
-		_score4 = _board.showScore(4).size();
+		if(_score1!=-1){_score1 = _board.showScore(1).size();}
+		if(_score2!=-1){_score2 = _board.showScore(2).size();}
+		if(_score3!=-1){_score3 = _board.showScore(3).size();}
+		if(_score4!=-1){_score4 = _board.showScore(4).size();}
 	}
 
 }

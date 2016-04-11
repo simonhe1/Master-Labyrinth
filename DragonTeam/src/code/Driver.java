85eb69f1f421277a35b08907a0d943172f75951d
david_000 <david_000@david-PC>
2016-04-11 09:21:47 -0400

    When user NO is not between 1 to 4, show warning.

--- DragonTeam/src/code/Driver.java
+++ DragonTeam/src/code/Driver.java
@@ -6,7 +6,12 @@ import gui.MultiLayers;
 
 public class Driver {
 	public static void main(String[]args){	
+		if(args.length>=1&&args.length<=4){
 		MultiLayers ml = new MultiLayers(args);
 		ml.setVisible(true);
+		}
+		else{
+		System.out.println("That's not the right amount of players!");
+		}
 	}
 }
